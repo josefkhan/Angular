a866720f56d0d4e31cdfcc20bd6644a4b2cfe644
Josef Khan <josef.i.khan@gmail.com>
2020-09-16 09:00:38 +0100

    Adminusers

---
+++ src/app/admin/adminuser/adminuser.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminuserRoutingModule } from './adminuser-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdminuserRoutingModule
+  ]
+})
+export class AdminuserModule { }
